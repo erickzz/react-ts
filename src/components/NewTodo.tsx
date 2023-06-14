a01121abf970596b3cdbfc57be7dfb0881d2e1d3
erickzz <erick.reis2@gmail.com>
2023-06-14 12:15:59 -0300

    New Todo working

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,12 +1,23 @@
-const NewTodo: React.FC = () => {
+import { useRef } from 'react';
 
-    const submitHandler = (event:React.FormEvent) => {
-        event.preventDefault()
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText?.trim().length === 0) {
+      //throw an error
+      return;
     }
+
+    props.onAddTodo(enteredText);
+  };
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo Text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInputRef} required />
       <button>Add Todo</button>
     </form>
   );
